9dc99a797691611839bdef81d00fb80c1ac29d93
wb_jinzhengjun <wb_jinzhengjun@meituan.com>
2023-06-16 15:04:10 +0800

    CompositeByteBuf

--- src/main/java/cn/jzj/netty/c1/netty/byteBuf/TestCompositeByteBuf.java
+++ src/main/java/cn/jzj/netty/c1/netty/byteBuf/TestCompositeByteBuf.java
@@ -1,10 +1,43 @@
 package cn.jzj.netty.c1.netty.byteBuf;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
+import io.netty.buffer.CompositeByteBuf;
 import lombok.extern.slf4j.Slf4j;
+
+import static io.netty.buffer.ByteBufUtil.appendPrettyHexDump;
+import static io.netty.util.internal.StringUtil.NEWLINE;
 
 @Slf4j
 public class TestCompositeByteBuf {
     public static void main(String[] args) {
+        ByteBuf buf1 = ByteBufAllocator.DEFAULT.buffer();
+        buf1.writeBytes(new byte[]{1, 2, 3, 4, 5});
+        ByteBuf buf2 = ByteBufAllocator.DEFAULT.buffer();
+        buf2.writeBytes(new byte[]{6, 7, 8, 9, 10});
+        /**
+         *         ByteBuf buffer = ByteBufAllocator.DEFAULT.buffer();
+         *         buffer.writeBytes(buf1).writeBytes(buf2);
+         *         log(buffer);
+         *         这个方法虽然能合并buf，但是效率不高
+         * **/
 
+        CompositeByteBuf buffer = ByteBufAllocator.DEFAULT.compositeBuffer();
+        buffer.addComponents(true, buf1, buf2);//设置这个参数为true才会写入新的buf
+        log(buffer);
+
+
+    }
+
+    private static void log(ByteBuf buffer) {
+        int length = buffer.readableBytes();
+        int rows = length / 16 + (length % 15 == 0 ? 0 : 1) + 4;
+        StringBuilder buf = new StringBuilder(rows * 80 * 2)
+                .append(" read index:").append(buffer.readerIndex())
+                .append(" write index:").append(buffer.writerIndex())
+                .append(" capacity: ").append(buffer.capacity())
+                .append(NEWLINE);
+        appendPrettyHexDump(buf,buffer);
+        System.out.println(buf.toString());
     }
 }
