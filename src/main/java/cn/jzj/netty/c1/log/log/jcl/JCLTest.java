893f9591a10c4113fc40a70d5f5b5621002913c1
wb_jinzhengjun <wb_jinzhengjun@meituan.com>
2023-06-20 16:19:23 +0800

    Log: JCL，apache的JUl，log4j等日志门面技术

---
+++ src/main/java/cn/jzj/netty/c1/log/log/jcl/JCLTest.java
@@ -0,0 +1,18 @@
+package cn.jzj.netty.c1.log.log.jcl;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ *  Log
+ *  LogFactory
+ *  若没有引用log4j，则默认使用jul，若引用log4j，则使用log4j，面向接口开发
+ */
+public class JCLTest {
+    public static void main(String[] args) {
+        //获取 log日志记录器对象
+        Log log = LogFactory.getLog(JCLTest.class);
+        //日志记录的输出
+        log.info("hello jcl");
+    }
+}
